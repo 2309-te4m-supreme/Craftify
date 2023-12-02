2cb7273c7ace3ac2a8548e338d53ec3b37f08370
Miranda Lucas <mirandalucas@Mirandas-MacBook-Pro.local>
2023-12-01 21:11:35 -0600

    single product page

--- src/client/components/Navigation.jsx
+++ src/client/components/Navigation.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function Navigation(){
   return(
     <nav className='nav-bar'>
-     <Link to='/'>Home</Link>
+     <Link to='/products'>Home</Link>
      <Link to='/login'>Login</Link>
     </nav>
   )
---
+++ src/client/components/SingleProduct.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+export default function SingleProduct() {
+
+    const [productDetails, setProductDetails] = useState({})
+    const API = 'http://localhost:3000/api'
+
+    let { productId } = useParams()
+
+    useEffect(() => {
+        getSingleProduct()
+    }, [productId])
+
+    async function getSingleProduct() {
+        try {
+            const response = await fetch(`${API}/products/${productId}`)
+            const result = await response.json()
+
+            setProductDetails(result[0])
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    // look at reserveBook function for 'add to cart'
+
+    return (
+        <>
+        <div>
+        <section key={productDetails.product_id} className='single-product'>
+                    <h3>{productDetails.product_name}</h3>
+                    <img src={productDetails.product_image} />
+                    <p>{productDetails.description}</p>
+                    <p>${productDetails.product_price}</p>
+                        <button>Add to cart</button>
+                </section>
+        </div>
+        </>
+    )
+}
