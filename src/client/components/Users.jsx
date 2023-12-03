5404ad12eb1e8b97a10d8dd9c9552bb58fa4e802
MMiller918 <mason.miller91@yahoo.com>
2023-12-03 13:53:26 -0600

    added functionality to users

--- src/client/components/Users.jsx
+++ src/client/components/Users.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react"
 import { Link } from 'react-router-dom'
 
 function Users (){
-  
   const [ users, setUsers ] = useState([]);
+  const [ selectedUsers, setSelectedUsers ] = useState({});
 
   useEffect(() => {
     fetchUsers()
@@ -17,20 +17,34 @@ function Users (){
       const response = await fetch(`${API}/users`)
       const result = await response.json()
       console.log(result.users)
+      const initialSelectedUsers = result.users.reduce((acc, user) => {
+        acc[user.user_id] = false;
+        return acc;
+      }, {});
+      setSelectedUsers(initialSelectedUsers);
       setUsers(result.users)
     } 
     catch(err){
       console.error(err.message)
     }
   } 
-    return (
-      <div>
-        <h1>Users:</h1>
-        <ul>
-        {
-          users.map((user) => (
-            <li key={user.user_id}>
-              <h3>{user.first_name} {user.last_name}</h3>
+
+  const handleUserClick = (userId) => {
+    setSelectedUsers((prevSelectedUsers) => ({
+      ...prevSelectedUsers,
+      [userId]: !prevSelectedUsers[userId],
+    }));
+  };
+  return (
+    <div>
+      <h1>Users:</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.user_id}>
+            <h3 onClick={() => handleUserClick(user.user_id)} style={{ cursor: 'pointer' }}>
+              {user.first_name} {user.last_name}
+            </h3>
+            {selectedUsers[user.user_id] && (
               <ul>
                 <li>Email: {user.email}</li>
                 <li>Address: {user.address}</li>
@@ -38,11 +52,12 @@ function Users (){
                 <li>Permissions: {user.permissions}</li>
                 <li>User ID: {user.users_id}</li>
               </ul>
-          </li> 
+            )}
+          </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default Users
